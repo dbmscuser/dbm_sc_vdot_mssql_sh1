1ed03c89983accc9f4bf9ff3e77578b43afcc546
dbmscuser <dbmscuser>
2024-06-18 16:50:20 +0000

    Commit

--- dbo/Tables/Creation-Scripts/Table_1.sql
+++ dbo/Tables/Creation-Scripts/Table_1.sql
@@ -3,6 +3,6 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Table_1](
-	[test] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+	[col1] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 )
 GO
