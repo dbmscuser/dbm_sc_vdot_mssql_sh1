395b605d5a8639527684283fea091428d0216610
dbmscuser <dbmscuser>
2024-06-19 13:20:11 +0000

    add col2 to table_1

--- dbo/Tables/Creation-Scripts/Table_1.sql
+++ dbo/Tables/Creation-Scripts/Table_1.sql
@@ -3,6 +3,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Table_1](
-	[col1] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+	[col1] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[col2] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 )
 GO
