22fc68b70af50fc7dfb7efe20b83d511f7e632b8
dbmscuser <dbmscuser>
2024-06-20 14:47:57 +0000

    Changed Data Col2

--- dbo/Tables/Creation-Scripts/Table_2.sql
+++ dbo/Tables/Creation-Scripts/Table_2.sql
@@ -4,6 +4,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Table_2](
 	[col1] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	[col2] [int] NULL,
  CONSTRAINT [pk_table_1_col1] PRIMARY KEY CLUSTERED 
 (
 	[col1] ASC
